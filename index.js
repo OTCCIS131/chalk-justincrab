8cc58b1997353147eaf37edc781105f63ba31ad1
Justin Crabtree <justincrab@gmail.com>
2017-09-06 13:14:01 -0500

    making colors
    happy bday

---
+++ index.js
@@ -0,0 +1,15 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Happy') + chalk.red(' Birthday'));
+console.log(chalk.bgMagenta('To You'));
+console.log(chalk.bgGreen('Happy friggin') + chalk.yellow(' Birthday ') + chalk.bgCyan('to youuuuu'));
+console.log(chalk.yellow('Happy') + chalk.magenta(' Birthday') + chalk.red(' dear sensei'));
+console.log(chalk.blue('Happy ')+chalk.green('Birthday')+chalk.yellow(' to')+chalk.magenta(' YOOOOOOUUUUUUU!!!'));
+console.log("");
+console.log(chalk.bold.blue("THE STORY SO FAR"));
+console.log(chalk.green('There once was a teacher'));
+console.log(chalk.yellow('he was a new teacher'));
+console.log(chalk.magenta('who had too many programs'));
+console.log(chalk.red('and his students drank too much covfefe'));
+console.log(chalk.green('so then they left class to pee too often'));
+console.log(chalk.bold.white('anime is good'));
